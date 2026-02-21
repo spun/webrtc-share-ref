e1f378beaed56815870914037c6afa51b06a4125
spundev <spundev@gmail.com>
2026-02-21 22:18:43 +0100

    Implement clipboard copy/paste for room ids - Switch to alpha compose BoM to use new button size APIs.

--- client_android/app/src/main/java/com/spundev/webrtcshare/ui/screens/createRoom/CreateRoomScreen.kt
+++ client_android/app/src/main/java/com/spundev/webrtcshare/ui/screens/createRoom/CreateRoomScreen.kt
@@ -1,20 +1,33 @@
 package com.spundev.webrtcshare.ui.screens.createRoom
 
+import android.content.ClipData
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import timber.log.Timber
+import com.spundev.webrtcshare.R
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun CreateRoomRoute(
     onNavigateBack: () -> Unit,
@@ -23,6 +36,10 @@ fun CreateRoomRoute(
     val isConnected by viewModel.isConnected.collectAsStateWithLifecycle()
     val roomId by viewModel.roomId.collectAsStateWithLifecycle()
     val messages by viewModel.messages.collectAsStateWithLifecycle()
+
+    val scope = rememberCoroutineScope()
+    val clipboard = LocalClipboard.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -31,12 +48,29 @@ fun CreateRoomRoute(
         Text("isConnected: $isConnected")
         Row(verticalAlignment = Alignment.CenterVertically) {
             Text("roomId: $roomId")
-            TextButton(onClick = {
-                Timber.d("TODO: Copy room id $roomId")
-            }) {
-                Text("Copy (TODO)")
+
+            // Copy to clipboard buton
+            val size = ButtonDefaults.ExtraSmallContainerHeight
+            OutlinedButton(
+                onClick = {
+                    val clipData = ClipData.newPlainText("Room id", roomId)
+                    val clipEntry = ClipEntry(clipData)
+                    scope.launch { clipboard.setClipEntry(clipEntry) }
+                },
+                contentPadding = ButtonDefaults.contentPaddingFor(size),
+                modifier = Modifier.heightIn(size),
+            ) {
+                Icon(
+                    painterResource(R.drawable.ic_content_copy_24),
+                    contentDescription = "Copy room id",
+                    modifier = Modifier.size(ButtonDefaults.iconSizeFor(size)),
+                )
+                Spacer(Modifier.size(ButtonDefaults.iconSpacingFor(size)))
+                Text("Copy")
             }
         }
+
+        // Send message button
         Button(
             enabled = isConnected,
             onClick = { viewModel.sendMessage("Eagle") }
