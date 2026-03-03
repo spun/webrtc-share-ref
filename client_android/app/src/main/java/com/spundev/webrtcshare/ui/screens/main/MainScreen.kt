93890f46cb7b21a5aa968a37e1481d97d05e5f9e
spundev <spundev@gmail.com>
2026-03-03 15:52:33 +0100

    Modify MainScreen content to make it scrollable

--- client_android/app/src/main/java/com/spundev/webrtcshare/ui/screens/main/MainScreen.kt
+++ client_android/app/src/main/java/com/spundev/webrtcshare/ui/screens/main/MainScreen.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -50,7 +52,9 @@ fun MainRoute(
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
             Column(
                 verticalArrangement = Arrangement.spacedBy(2.dp),
